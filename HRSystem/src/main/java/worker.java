76ea675eb93232043d49910d119b2aebdef2af6c
shilinzhang-cs <75181480+shilinzhang-cs@users.noreply.github.com>
2021-10-13 11:51:03 -0400

    new version with inheritance

--- HRSystem/src/main/java/worker.java
+++ HRSystem/src/main/java/worker.java
@@ -1,32 +1,15 @@
-public class worker {
-    private final String name;
+public class worker extends departmentHead{
     private double salary;
-    private final int ID;
-    private final String department;
     private final schedule schedule;
 
     public worker(String name, int salary, int ID, String department, schedule schedule){
-        this.name = name;
+        super(name, ID, department);
         this.salary = salary;
-        this.ID = ID;
-        this.department = department;
         this.schedule = schedule;
-    }
-
-    public String getName(){
-        return this.name;
     }
 
     public double getSalary(){
         return this.salary;
-    }
-
-    public int getID(){
-        return this.ID;
-    }
-
-    public String getDepartment(){
-        return this.department;
     }
 
     public void setSalary(double salary){
