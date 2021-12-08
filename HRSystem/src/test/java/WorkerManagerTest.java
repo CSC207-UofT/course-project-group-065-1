a8dbf13d98d0e94fdaf9ad4a05e88a0d08a4bfcd
Ruixin Liu <ruixin.liu@mail.utoronto.ca>
2021-12-08 18:13:08 -0500

    New test cases added

--- HRSystem/src/test/java/WorkerManagerTest.java
+++ HRSystem/src/test/java/WorkerManagerTest.java
@@ -29,7 +29,21 @@ public class WorkerManagerTest {
     public void testChangeSalary() throws IOException, ClassNotFoundException {
         WorkerReadWriter readWriter = new WorkerReadWriter();
         manager = new WorkerManager(readWriter, true);
-        String output = manager.changeSalary(0, 0.5, true).get(0);
+        ArrayList<String> WorkerArguments = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(WorkerArguments, true);
+        String output = manager.changeSalary(0, 0.5, true).get(1);
+        String expected = "S Bob 0 3000.0";
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testChangeSalaryNull() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(arguments, true);
+        String output = manager.changeSalary(1, 0.5, true).get(0);
         String expected = "N I";
         manager.deleteAllWorker(true);
         Assertions.assertEquals(expected, output);
@@ -49,6 +63,20 @@ public class WorkerManagerTest {
     }
 
     @Test
+    public void testChangeScheduleNull() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> WorkerArguments = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(WorkerArguments, true);
+        ArrayList<String> arguments = new ArrayList<>(List.of("1", "Tuesday", "11AM", "9PM"));
+        ArrayList<String> output = manager.changeSchedule(arguments, true);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("N I");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
     public void testDeleteWorker() throws IOException, ClassNotFoundException {
         WorkerReadWriter readWriter = new WorkerReadWriter();
         manager = new WorkerManager(readWriter, true);
@@ -56,6 +84,19 @@ public class WorkerManagerTest {
         manager.createWorker(arguments, true);
         String output = manager.deleteWorker(0, true).get(1);
         String expected = "S Bob 0 bakery 2000.0 Monday from 9AM to 4PM";
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testDeleteWorkerNull() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(arguments, true);
+        ArrayList<String> output = manager.deleteWorker(10, true);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("N I");
         manager.deleteAllWorker(true);
         Assertions.assertEquals(expected, output);
     }
@@ -77,6 +118,20 @@ public class WorkerManagerTest {
     }
 
     @Test
+    public void testSearchByNameNull() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments1 = new ArrayList<>(List.of("Bob", "3000", "clothes", "Friday", "1AM", "10PM"));
+        manager.createWorker(arguments1, true);
+        ArrayList<String> arguments2 = new ArrayList<>(List.of("Bob", "5000", "clothes", "Friday", "3AM", "6PM"));
+        manager.createWorker(arguments2, true);
+        ArrayList<String> output = manager.searchByName("Mike");
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("f");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+    @Test
     public void testAllWorker() throws IOException, ClassNotFoundException {
         WorkerReadWriter readWriter = new WorkerReadWriter();
         manager = new WorkerManager(readWriter, true);
@@ -88,6 +143,84 @@ public class WorkerManagerTest {
         ArrayList<String> expected = new ArrayList<>();
         expected.add("Bob 0 bakery 1900.0 Monday from 9AM to 4PM");
         expected.add("Victor 1 clothes 3000.0 Friday from 1AM to 10PM");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testSearchById() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments1 = new ArrayList<>(List.of("Bob", "3000", "clothes", "Friday", "1AM", "10PM"));
+        manager.createWorker(arguments1, true);
+        ArrayList<String> arguments2 = new ArrayList<>(List.of("Tom", "5000", "clothes", "Friday", "3AM", "6PM"));
+        manager.createWorker(arguments2, true);
+        ArrayList<String> output = manager.searchByID(1);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Tom 1 clothes 5000.0 Friday from 3AM to 6PM");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testSearchByIdNull() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments1 = new ArrayList<>(List.of("Bob", "3000", "clothes", "Friday", "1AM", "10PM"));
+        manager.createWorker(arguments1, true);
+        ArrayList<String> arguments2 = new ArrayList<>(List.of("Tom", "5000", "clothes", "Friday", "3AM", "6PM"));
+        manager.createWorker(arguments2, true);
+        ArrayList<String> output = manager.searchByID(10);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("F");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testUndoCreateWorker() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> arguments1 = new ArrayList<>(List.of("Bob", "1900", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(arguments1, true);
+        ArrayList<String> arguments2 = new ArrayList<>(List.of("Victor", "3000", "clothes", "Friday", "1AM", "10PM"));
+        manager.createWorker(arguments2, true);
+        manager.undoCreateWorker(true);
+        ArrayList<String> output = manager.allWorker();
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Bob 0 bakery 1900.0 Monday from 9AM to 4PM");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+
+    @Test
+    public void testUndoDeleteWorker() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> argument1 = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        ArrayList<String> argument2 = new ArrayList<>(List.of("Bob", "2000", "0", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(argument1, true);
+        manager.deleteWorker(0, true);
+        manager.undoDeleteWorker(argument2, true);
+        ArrayList<String> output = manager.allWorker();
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Bob 0 bakery 2000.0 Monday from 9AM to 4PM");
+        manager.deleteAllWorker(true);
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testUndoChangeSalary() throws IOException, ClassNotFoundException {
+        WorkerReadWriter readWriter = new WorkerReadWriter();
+        manager = new WorkerManager(readWriter, true);
+        ArrayList<String> WorkerArguments = new ArrayList<>(List.of("Bob", "2000", "bakery", "Monday", "9AM", "4PM"));
+        manager.createWorker(WorkerArguments, true);
+        manager.changeSalary(0, 0.5, true);
+        manager.undoChangeSalary(0, 2000, true);
+        ArrayList<String> output = manager.allWorker();
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Bob 0 bakery 2000.0 Monday from 9AM to 4PM");
         manager.deleteAllWorker(true);
         Assertions.assertEquals(expected, output);
     }
